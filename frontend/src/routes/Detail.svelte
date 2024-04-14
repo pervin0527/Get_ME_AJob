6fda635ba977b9188286b8b91dc9d31ba3222e82
김홍석 <kimhongseok@gimhongseog-ui-MacBookPro.local>
2024-04-14 23:08:07 +0900

    서버에서 데이터 가져오는거 테스트

--- frontend/src/routes/Detail.svelte
+++ frontend/src/routes/Detail.svelte
@@ -5,10 +5,18 @@
     let data_id = params.data_id
     let detail = {}
 
-    function get_detail() {
-        fastapi("get", "/api/data/data_detail/" + data_id, {}, (json) => {
-            detail = json
+    function get_detail() {        
+        //fastapi("get", "/api/data/data_detail/" + data_id, {}, (json) => {
+            //detail = json
+        //})
+        
+
+        fetch(`http://13.125.59.124:8000/api/data/data_detail/${data_id}`).then((response) => {
+            response.json().then((json) => {
+                detail = json
+            })
         })
+
     }
 
     get_detail()
--- frontend/src/routes/Field.svelte
+++ frontend/src/routes/Field.svelte
@@ -3,8 +3,14 @@
     let field_anal_list = []
 
     function get_field_anal_list() {
-        fastapi('get', '/api/data/field_anal', {}, (json) => {
-            field_anal_list = json
+        //fastapi('get', '/api/data/field_anal', {}, (json) => {
+            //field_anal_list = json
+        //})
+
+        fetch("http://13.125.59.124:8000/api/data/field_anal").then((response) => {
+            response.json().then((json) => {
+                field_anal_list = json
+            })
         })
     }
 
