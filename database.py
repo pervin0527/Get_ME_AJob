57138b8e4bd70799b228b4ab4eba6ab23ababd39
김홍석 <kimhongseok@gimhongseog-ui-MacBookPro.local>
2024-04-15 09:10:06 +0900

    db변경

--- database.py
+++ database.py
@@ -4,11 +4,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./getmeajob.db"
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:greedjar74!@database-1.cbsam62kgt7x.ap-northeast-2.rds.amazonaws.com/getmeajob'
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+if SQLALCHEMY_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
